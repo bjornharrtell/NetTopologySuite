999dbce2ad3b7c68ecb0d19651e45402c4e0b45d
Felix Obermaier <felix.obermaier@netcologne.de>
2020-11-12 11:48:17 +0100

    Add possibility to change coordinate equality test
    * Add and wire CoordinateEqualityComparer
    
    relates to #447

---
+++ test/NetTopologySuite.Samples.Console/Tests/Github/Issue447.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace NetTopologySuite.Samples.Tests.Github
+{
+    [Description("https://github.com/NetTopologySuite/NetTopologySuite/issues/447")]
+    public class Issue447
+    {
+        [Test]
+        public void Test()
+        {
+            NetTopologySuite.Geometries.Geometry.CoordinateEqualityComparer = new NetTopologySuite.Geometries.CoordinateZComparer();
+
+            Assert.IsFalse(new NetTopologySuite.Geometries.Point(1, 2) == new NetTopologySuite.Geometries.Point(1, 2, 3));
+            Assert.AreNotEqual(new NetTopologySuite.Geometries.Point(1, 2), new NetTopologySuite.Geometries.Point(1, 2, 3));
+
+            NetTopologySuite.Geometries.Geometry.CoordinateEqualityComparer = null;
+
+        }
+    }
+}
