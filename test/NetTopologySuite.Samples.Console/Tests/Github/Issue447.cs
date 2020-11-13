a65a8b5b06a6afae9ec4827b3fa64c08bc12836a
Felix Obermaier <felix.obermaier@netcologne.de>
2020-11-13 12:41:14 +0100

    Modify CoordinateEqualityComparer
    * Derive from EqualityComparer<Coordinate>,  implement sealed Equals
    * Add CoordinateZMComparer
    * Add airbreathers suggestion to modify Equal for testing purposes

--- test/NetTopologySuite.Samples.Console/Tests/Github/Issue447.cs
+++ test/NetTopologySuite.Samples.Console/Tests/Github/Issue447.cs
@@ -8,12 +8,22 @@ namespace NetTopologySuite.Samples.Tests.Github
         [Test]
         public void Test()
         {
-            NetTopologySuite.Geometries.Geometry.CoordinateEqualityComparer = new NetTopologySuite.Geometries.CoordinateZComparer();
+            NetTopologySuite.Geometries.Geometry.CoordinateEqualityComparer = new NetTopologySuite.Geometries.CoordinateZMComparer();
 
             Assert.IsFalse(new NetTopologySuite.Geometries.Point(1, 2) == new NetTopologySuite.Geometries.Point(1, 2, 3));
             Assert.AreNotEqual(new NetTopologySuite.Geometries.Point(1, 2), new NetTopologySuite.Geometries.Point(1, 2, 3));
 
             NetTopologySuite.Geometries.Geometry.CoordinateEqualityComparer = null;
+
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var pt1 = new NetTopologySuite.Geometries.Point(new NetTopologySuite.Geometries.Coordinate(10, 10));
+            var pt2 = new NetTopologySuite.Geometries.Point(new NetTopologySuite.Geometries.CoordinateZ(10, 10));
+            Assert.IsTrue(pt1 == pt2); 
+            Assert.IsTrue(pt1.Equals((object)pt2));
 
         }
     }
